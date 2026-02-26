5c981909ba5d261575c0b6a73a1cf6c2761ca2a6
Brian Caldera <briancaldera07@gmail.com>
2026-02-26 15:33:39 -0400

    🚚 refactor: move supabase client

--- src/components/organisms/Calculator.tsx
+++ src/components/organisms/Calculator.tsx
@@ -6,7 +6,7 @@ import { useQuery } from '@tanstack/react-query'
 import { isPresent, objectEntries } from 'ts-extras'
 import { setDefaultOptions } from 'date-fns'
 import { es } from 'date-fns/locale'
-import { supabaseClient } from '@/lib/data/supabase-client'
+import { supabaseClient } from '@/lib/supabase/client'
 import { currencyInfo } from '@/types/currency'
 
 setDefaultOptions({ locale: es })
